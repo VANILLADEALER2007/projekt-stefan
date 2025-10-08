fbbf8809eccc69b71c4ba1aeb6da774a4096f192
kubus323f <jakub.zietarski1@gmail.com>
2025-10-08 20:55:46 +0200

    Update main.h

--- projekt/main.h
+++ projekt/main.h
@@ -1,14 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-// Declarations based on main2.cpp
-// Add your includes here if needed
-
-// Example: If main2.cpp has functions like:
-// int add(int a, int b);
-// void printMessage(const std::string& msg);
-
-int add(int a, int b);
-void printMessage(const std::string& msg);
-
 #endif // MAIN_H
